6292ceb465864621c4ed583d90473f15d8d36f16
taballa <taballa@gmail.com>
2012-09-15 16:33:03 +0800

    zzz

--- 1/controller/show.class.php
+++ 1/controller/show.class.php
@@ -3,5 +3,5 @@ function show(){
 	$uid =  intval($_REQUEST['uid']);
 	if( $uid < 1 ) return info_page(‘错误的uid’);
 	$data['user'] = get_user_info_by_id(  $uid  );
-	render( $data );
+render( $data );
 }
