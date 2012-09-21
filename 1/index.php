c37a124cb2008e01b94687f4958496ecac9e4d72
taballa@gmail.com <taballa@gmail.com@b3e3cd90-e9d6-4b81-a7f8-c2484d8dfc40>
2012-09-21 09:48:35 +0000

    x
    git-svn-id: https://svn.sinaapp.com/stew@7 b3e3cd90-e9d6-4b81-a7f8-c2484d8dfc40

--- 1/index.php
+++ 1/index.php
@@ -1,5 +1,4 @@
 <?php
-
 /* lp app root */
 // ↑____ for aoi . Do Not Delete it.
 /****  load lp framework  ***/
@@ -9,4 +8,3 @@ define( 'AROOT' , dirname( __FILE__ ) . DS  );
 //ini_set('include_path', dirname( __FILE__ ) . DS .'_lp' ); 
 include_once( '_lp'.DS .'lp.init.php' );
 /**** lp framework init finished ***/
-
