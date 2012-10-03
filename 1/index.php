8fff8eb9cc5bbace3eaf31d8c45e11382c458305
taballa <taballa@gmail.com>
2012-10-03 22:09:05 +0800

    test git add -p

--- 1/index.php
+++ 1/index.php
@@ -9,4 +9,3 @@ define( 'AROOT' , dirname( __FILE__ ) . DS  );
 //ini_set('include_path', dirname( __FILE__ ) . DS .'_lp' ); 
 include_once( '_lp'.DS .'lp.init.php' );
 /**** lp framework init finished ***/
-
